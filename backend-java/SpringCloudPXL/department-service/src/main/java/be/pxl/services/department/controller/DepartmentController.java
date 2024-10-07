1af727876ec716b27089d432e3e44478b868c93f
AngelChezziPXL <angel.chezzi@student.pxl.be>
2024-10-08 00:49:18 +0200

    Department package added. To be finalized

---
+++ backend-java/SpringCloudPXL/department-service/src/main/java/be/pxl/services/department/controller/DepartmentController.java
@@ -0,0 +1,9 @@
+package be.pxl.services.department.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/department")
+public class DepartmentController {
+}
