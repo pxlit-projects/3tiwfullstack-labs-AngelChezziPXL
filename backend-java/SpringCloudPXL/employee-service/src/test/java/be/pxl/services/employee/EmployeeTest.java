b52464abb4089dbf6b2e936271f0578142343a87
AngelChezziPXL <angel.chezzi@student.pxl.be>
2024-10-22 16:02:02 +0200

    Test for get request added, but is not working yet

--- backend-java/SpringCloudPXL/employee-service/src/test/java/be/pxl/services/employee/EmployeeTest.java
+++ backend-java/SpringCloudPXL/employee-service/src/test/java/be/pxl/services/employee/EmployeeTest.java
@@ -2,7 +2,10 @@ package be.pxl.services.employee;
 
 import be.pxl.services.employee.domain.Employee;
 import be.pxl.services.employee.repository.EmployeeRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,10 +14,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -59,6 +66,42 @@ public class EmployeeTest {
                 .andExpect(status().isCreated());
 
         assertEquals(1, employeeRepository.findAll().size());
+    }
+
+    @Test
+    public  void testGetEmployees() throws Exception {
+        List<Employee> seedEmployees = new ArrayList<>();
+        seedEmployees.add(Employee.builder().age(22).name("John").position("student").build());
+        seedEmployees.add(Employee.builder().age(35).name("Piet").position("teacher").build());
+        seedEmployees.add(Employee.builder().age(32).name("Luc").position("teacher").build());
+        seedEmployees.add(Employee.builder().age(21).name("Jef").position("student").build());
+        seedEmployees.add(Employee.builder().age(20).name("Lisa").position("student").build());
+
+        for (Employee employee : seedEmployees) {
+            addEmployeesToMockDatabase(employee);
+        }
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseContent = result.getResponse().getContentAsString();
+        List<Employee> responseEmployees = objectMapper.readValue(responseContent, new TypeReference<List<Employee>>() {
+        });
+
+        assertEquals(seedEmployees, responseEmployees);
+
+    }
+
+    //Helper methods
+    public void addEmployeesToMockDatabase(Employee employee) throws Exception {
+        String employeeString = objectMapper.writeValueAsString(employee);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/employee")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(employeeString))
+                .andExpect(status().isCreated());
     }
 
 }
