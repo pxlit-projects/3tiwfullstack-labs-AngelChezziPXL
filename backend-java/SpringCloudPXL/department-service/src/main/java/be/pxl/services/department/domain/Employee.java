af705219212adf198045d208fce1655d3d8ebfc0
AngelChezziPXL <angel.chezzi@student.pxl.be>
2024-10-22 12:15:49 +0200

    Annotaions added to be able to make an object and have getters and setters

--- backend-java/SpringCloudPXL/department-service/src/main/java/be/pxl/services/department/domain/Employee.java
+++ backend-java/SpringCloudPXL/department-service/src/main/java/be/pxl/services/department/domain/Employee.java
@@ -1,5 +1,14 @@
 package be.pxl.services.department.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
     private Long id;
     private Long organisationId;
