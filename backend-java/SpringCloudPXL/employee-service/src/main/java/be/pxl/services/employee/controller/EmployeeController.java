7b2acceab394842269f33a625e36bd73daef3a08
AngelChezziPXL <angel.chezzi@student.pxl.be>
2024-10-22 12:11:54 +0200

    remove unused imports

--- backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/employee/controller/EmployeeController.java
+++ backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/employee/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import be.pxl.services.employee.domain.Employee;
 import be.pxl.services.employee.domain.dto.EmployeeRequest;
 import be.pxl.services.employee.services.IEmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
