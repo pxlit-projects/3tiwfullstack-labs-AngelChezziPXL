d803c9fbafa8691041fb346d2cbf2ac446be377a
AngelChezziPXL <angel.chezzi@student.pxl.be>
2024-10-25 00:17:02 +0200

    File prepared with ToDos to write later tests

---
+++ backend-java/SpringCloudPXL/organization-service/src/test/java/be/pxl/services/organization/OrganizationTest.java
@@ -0,0 +1,22 @@
+package be.pxl.services.organization;
+
+import org.testcontainers.shaded.org.apache.commons.lang3.NotImplementedException;
+//TODO: add all annotations and missing parts for this class
+
+public class OrganizationTest {
+
+    public void testGetOrganizationById() throws Exception {
+        //TODO: Add test for 'public OrganizationRespond getOrganizationById'
+        throw new NotImplementedException();
+    }
+
+    public void testGetOrganizationByIdWithDepartments() throws Exception {
+        //TODO: write test for 'public OrganizationRespond getOrganizationByIdWithDepartments'
+        throw new NotImplementedException();
+    }
+
+    public void testGetOrganizationByIdWithEmployees() throws Exception {
+        //TODO: write test for 'public OrganizationRespond getOrganizationByIdWithEmployees'
+    }
+
+}
