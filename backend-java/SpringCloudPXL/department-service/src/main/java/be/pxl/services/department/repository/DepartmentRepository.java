043ddd13ab0977c5a8d8f654964fddc2240d0add
AngelChezziPXL <angel.chezzi@student.pxl.be>
2024-10-25 00:15:12 +0200

    commented out query of employees to enable testing. Must be repaired in later Lab

--- backend-java/SpringCloudPXL/department-service/src/main/java/be/pxl/services/department/repository/DepartmentRepository.java
+++ backend-java/SpringCloudPXL/department-service/src/main/java/be/pxl/services/department/repository/DepartmentRepository.java
@@ -13,6 +13,6 @@ public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Optional<List<Department>> findAllByOrganizationId(Long organizationId);
 
     //TODO: Check query. Not working
-    @Query("SELECT d FROM Department d JOIN d.employees e WHERE d.organizationId = :organizationId GROUP BY d HAVING COUNT(e) > 0")
+    //@Query("SELECT d FROM Department d JOIN d.employees e WHERE d.organizationId = :organizationId GROUP BY d HAVING COUNT(e) > 0")
     Optional<List<Department>> findAllWithEmployeesByOrganizationId(Long organizationId);
 }
